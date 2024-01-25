df614f8237393047e43556255a40ecfc9e216cd3
Viktor Starodubtsev <viktorst2005@gmail.com>
2024-01-25 16:27:35 +0400

    Example added.

--- examples/elephant/main.rs
+++ examples/elephant/main.rs
@@ -4,8 +4,9 @@ use chaorder::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let illustration: Illustration =
-        ParseIllustration::from_txt(&std::path::Path::new("./foo.txt"));
+    let illustration: Illustration = ParseIllustration::from_txt(&std::path::Path::new(
+        "./examples/elephant/assets/elephant.txt",
+    ));
 
     let mut board = TerminalDrawBoard::from(illustration);
     board.draw()
