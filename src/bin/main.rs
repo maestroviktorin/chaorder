34b0ffe5657e604af1e003f38eaa31ddcc8f47f7
Viktor Starodubtsev <viktorst2005@gmail.com>
2024-01-25 15:22:20 +0400

    Illustration parsing from .txt implemented.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,40 +1,7 @@
-use chaorder::{draw::TerminalDrawBoard, illustration::Illustration};
-
-use std::collections::HashMap;
+use chaorder::{draw::TerminalDrawBoard, illustration::{Illustration, ParseIllustration}};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let illustration: Illustration = HashMap::from_iter(
-        vec![
-            ((0, 0), ' '),
-            ((0, 1), ' '),
-            ((0, 2), 'U'),
-            ((0, 3), 'R'),
-            ((0, 4), 'A'),
-            ((0, 5), 'L'),
-            ((0, 6), ' '),
-            ((0, 7), ' '),
-            ((0, 8), ' '),
-            ((1, 0), ' '),
-            ((1, 1), ' '),
-            ((1, 2), ' '),
-            ((1, 3), ' '),
-            ((1, 4), '/'),
-            ((1, 5), '\\'),
-            ((1, 6), ' '),
-            ((1, 7), ' '),
-            ((1, 8), ' '),
-            ((2, 0), ' '),
-            ((2, 1), '/'),
-            ((2, 2), '\\'),
-            ((2, 3), '/'),
-            ((2, 4), '\\'),
-            ((2, 5), '\\'),
-            ((2, 6), ' '),
-            ((2, 7), '\\'),
-            ((2, 8), ' '),
-        ]
-        .into_iter(),
-    );
+    let illustration: Illustration = ParseIllustration::from_txt(&std::path::Path::new("./assets/elephant.txt"));
 
     let mut board = TerminalDrawBoard::from(illustration);
     board.draw()
--- src/illustration.rs
+++ src/illustration.rs
@@ -1,5 +1,22 @@
 // Work in progress.
 
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::read_to_string, path::Path};
 
 pub type Illustration = HashMap<(u16, u16), char>;
+
+pub struct ParseIllustration;
+
+impl ParseIllustration {
+    pub fn from_txt(path: &Path) -> Illustration {
+        read_to_string(path)
+            .unwrap()
+            .lines()
+            .enumerate()
+            .flat_map(|(row, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(move |(column, chr)| ((row as u16, column as u16), chr))
+            })
+            .collect()
+    }
+}
