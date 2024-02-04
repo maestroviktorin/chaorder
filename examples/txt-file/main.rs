5a54664c23d869cbf7c4c499692c22899037b6c7
Viktor Starodubtsev <viktorst2005@gmail.com>
2024-02-05 01:00:54 +0400

    Core adaptation done.

--- examples/txt-file/main.rs
+++ examples/txt-file/main.rs
@@ -1,16 +1,39 @@
 use chaorder::{
-    draw::Drawer,
-    illustration::{Illustration, ParseIllustration},
+    draw_new::Illustrator,
+    illustration::ParseIllustration,
 };
+use crossterm::{
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    layout::Rect,
+    prelude::{CrosstermBackend, Terminal},
+    TerminalOptions, Viewport,
+};
+use std::io::{stdout, Result};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let illustration: Illustration =
-        ParseIllustration::from_txt(&std::path::Path::new("./examples/txt-file/assets/crab.txt"))
-            .unwrap();
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::with_options(
+        CrosstermBackend::new(stdout()),
+        TerminalOptions {
+            viewport: Viewport::Fixed(Rect::new(0, 0, 10, 2)),
+        },
+    )?;
+    terminal.clear()?;
 
-    let mut draw = Drawer::new(illustration)
-        .with_char_range(400)
-        .with_start((0, 9))
-        .build();
-    draw.run()
+    let illustration = ParseIllustration::from_str("1234567890\n0987654321");
+
+    terminal.draw(|frame| {
+        let area = frame.size();
+        let ill = illustration.clone();
+
+        frame.render_widget(Illustrator::new().illustration(ill).char_range(2).build(), area);
+    })?;
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
--- src/draw_new.rs
+++ src/draw_new.rs
@@ -2,11 +2,7 @@ use crate::illustration::Illustration;
 
 use rand::prelude::*;
 
-use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    widgets::Widget,
-};
+use ratatui::{buffer::Buffer, layout::Rect, widgets::Widget};
 
 use std::{
     collections::{HashMap, HashSet},
@@ -32,8 +28,53 @@ impl Illustrator {
 }
 
 impl Widget for Illustrator {
-    fn render(self, _: Rect, _: &mut Buffer) {
-        todo!()
+    fn render(self, _: Rect, buf: &mut Buffer) {
+        let mut rng = thread_rng();
+
+        // Storing `(u16, u16)`s instead of `Cell`s is cheaper
+        // and does not require other overheads associated with mutual exclusion
+        // and `derive`s for `Cell`.
+        //
+        // Also, accounting for "ready" `Cell`s
+        // is a functionality related only to the `draw` method.
+        let mut ready: HashSet<(u16, u16)> = HashSet::with_capacity(buf.area().area() as usize);
+
+        // FIXME.
+        let mut range_width_coefficients: HashMap<(u16, u16), u32> =
+            HashMap::from_iter((buf.area().left()..buf.area().right()).flat_map(|col| {
+                (buf.area().top()..buf.area().bottom()).map(move |row| ((col, row), 1))
+            }));
+
+        while ready.len() < self.illustration.len() {
+            thread::sleep(Duration::from_nanos(self.sleep_nanos));
+
+            let rect = buf.area();
+            let (col, row) = (
+                rng.gen_range(rect.left()..rect.right()) as u16,
+                rng.gen_range(rect.top()..rect.bottom()),
+            );
+
+            if ready.contains(&(col, row)) {
+                continue;
+            }
+
+            let required_char = self.illustration.get(&(row, col)).unwrap().to_owned();
+            let abs_vicinity = self.char_range / range_width_coefficients.get(&(col, row)).unwrap();
+            let (lower, upper) = (
+                (required_char as u32).saturating_sub(abs_vicinity),
+                (required_char as u32) + abs_vicinity,
+            );
+            let generated_char = char::from_u32(rng.gen_range(lower..=upper)).unwrap();
+
+            *range_width_coefficients.get_mut(&(col, row)).unwrap() *=
+                self.char_range_reduction_factor;
+
+            if generated_char == required_char {
+                ready.insert((col, row));
+            }
+
+            buf.get_mut(col, row).set_char(generated_char);
+        }
     }
 }
 
@@ -56,22 +97,22 @@ impl Default for IllustratorBuilder {
 }
 
 impl IllustratorBuilder {
-    pub fn with_illustration(mut self, illustration: Illustration) -> Self {
+    pub fn illustration(mut self, illustration: Illustration) -> Self {
         self.illustration = illustration;
         self
     }
 
-    pub fn with_sleep_nanos(mut self, sleep_nanos: u64) -> Self {
+    pub fn sleep_nanos(mut self, sleep_nanos: u64) -> Self {
         self.sleep_nanos = sleep_nanos;
         self
     }
 
-    pub fn with_char_range(mut self, char_range: u32) -> Self {
+    pub fn char_range(mut self, char_range: u32) -> Self {
         self.char_range = char_range;
         self
     }
 
-    pub fn with_char_range_reduction_factor(mut self, char_range_reduction_factor: u32) -> Self {
+    pub fn char_range_reduction_factor(mut self, char_range_reduction_factor: u32) -> Self {
         self.char_range_reduction_factor = char_range_reduction_factor;
         self
     }
